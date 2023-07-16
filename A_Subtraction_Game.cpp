e679b34416f941e8f662d265ea9210517fb458f8
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-07-16 16:27:56 +0600

    init

---
+++ A_Subtraction_Game.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int t;
+    std::cin >> t;
+
+    while (t--)
+    {
+        int a, b;
+        std::cin >> a >> b;
+
+        // Output any positive integer n such that the second player wins
+        std::cout << "2" << std::endl;
+    }
+
+    return 0;
+}
